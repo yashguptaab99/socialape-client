ad76ec0091fed37c5224b672d8d3f81129247556
yashguptaab99 <yashguptaab99@gmail.com>
2021-03-04 03:57:21 +0530

    first commit

---
+++ src/components/Layout/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Component, Fragment } from 'react'
+import {Link} from "react-router-dom";
+import { connect } from 'react-redux'
+import PropTypes from "prop-types";
+import MyButton from '../../util/MyButton';
+import PostScream from "../scream/PostScream";
+import Notifications from './Notifications';
+
+// MUI stuff
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+// Icons
+import HomeIcon from '@material-ui/icons/Home';
+
+
+export class Navbar extends Component {
+    render() {
+        const { authenticated } = this.props
+        return (
+            <div> 
+                <AppBar>
+                    <Toolbar className='nav-container'>
+                        { authenticated ? (
+                            <Fragment>
+                                <PostScream/>
+                                <Link to="/">
+                                    <MyButton
+                                        tip="Home"
+                                    >
+                                        <HomeIcon/>
+                                    </MyButton>
+                                </Link>
+                                <Notifications/>
+                            </Fragment>
+                        ) : (
+                                <Fragment>
+                                    <Button color = 'inherit'component = { Link } to = '/login'>Login</Button>
+                                    <Button color='inherit' component={Link} to='/'>Home</Button>
+                                    <Button color='inherit' component={Link} to='/signup'>Signup</Button>
+                                </Fragment>
+                            )
+                        }
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    authenticated: state.user.authenticated
+});
+
+Navbar.propTypes = {
+    authenticated: PropTypes.bool.isRequired
+}
+
+export default connect(mapStateToProps)(Navbar);
